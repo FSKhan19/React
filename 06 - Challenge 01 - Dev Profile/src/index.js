1daebbb053d555688498af7fb37073fad9e4634d
Farhan Shahid <farhanshahidkhan93@gmail.com>
2024-03-18 15:03:49 +0500

    Update

--- 06 - Challenge 01 - Dev Profile/src/index.js
+++ 06 - Challenge 01 - Dev Profile/src/index.js
@@ -1,5 +1,5 @@
 import React from "react";
-import ReactDOM from "react-dom/client";
+import ReactDOM from "react-dom/client.js";
 import profileData from "./data.js";
 import Card from "./Card.js"; // Import the Card component
 function App() {
